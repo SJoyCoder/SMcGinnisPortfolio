cca111a6fb3def223dfaad473364bf26bf6c8325
Savannah McGinnis <sjmhorsegirl@verizon.net>
2022-11-12 13:41:17 -0600

    Projects and Rseume pages

---
+++ src/pages/Projects.js
@@ -0,0 +1,62 @@
+import React from 'react';
+
+const Projects = () => {
+    return (
+        <section id="projects">
+        <h2>Projects</h2>
+        <ul>
+            <li id="showcase-project">
+                <h3>Happy Holiday Gift Tracker</h3>
+                <a href="https://safe-taiga-52542.herokuapp.com/">
+                <img src="./assets/HappyHolidaysGiftTracker.png" alt="Happy Holidays Gift Tracker Screenshot"/>
+                </a>
+                <a href="https://github.com/SJoyCoder/HappyHolidaysGiftTracker">Click here for Repo</a>
+            </li>
+            <li class="smaller-projects">
+                <h3>Movies R Us</h3>
+                <a href="https://drepel97.github.io/MoviesRUs/">
+                <img src="./assets/MoviesRUs.gif" alt="MoviesRUs Gif"/>
+                </a>
+                <a href="https://github.com/Drepel97/MoviesRUs">Click here for Repo</a>
+            </li>
+            <li class="smaller-projects">
+                <h3>Note Taker App</h3>
+                <a href="https://github.com/SJoyCoder/noteTakerApp">
+                <img src="./assets/Note Taker.gif" alt="Note Taker App Gif"/>
+                </a>
+                <a href="https://github.com/SJoyCoder/noteTakerApp">Click here for Repo</a>
+            </li>
+            <li class="smaller-projects">
+                <h3>Tech Blog</h3>
+                <a href="https://cryptic-lowlands-37976.herokuapp.com/">
+                <img src="./assets/TechBlog.png" alt="Tech Blog Screenshot"/>
+                </a>
+                <a href="https://github.com/SJoyCoder/TechBlog">Click here for Repo</a>
+            </li>
+            <li class="smaller-projects">
+                <h3>Team Roster Generator</h3>
+                <a href="https://sjoycoder.github.io/GenerateTeamRoster/">
+                <img src="./assets/GenerateTeamRoster.png" alt="Team Roster Screenshot"/>
+                </a>
+                <a href="https://github.com/SJoyCoder/GenerateTeamRoster">Click here for Repo</a>
+            </li>
+            <li class="smaller-projects">
+                <h3>Weather Dashboard</h3>
+                <a href="https://sjoycoder.github.io/Challenge6-WeatherDashboard/">
+                <img src="./assets/Weather Dashboard.gif" alt="Weather Dashboard"/>
+                </a>
+                <a href="https://github.com/SJoyCoder/GenerateTeamRoster">Click here for Repo</a>
+            </li>
+            <li class="smaller-projects">
+                <h3>Work Day Scheduler</h3>
+                <a href="https://sjoycoder.github.io/Challenge5-Calendar/">
+                <img src="./assets/Work Day Scheduler.gif" alt="Work Day Scheduler Gif"/>
+                </a>
+                <a href="https://github.com/SJoyCoder/Challenge5-Calendar">Click here for Repo</a>
+            </li>
+        </ul>
+    </section>
+    );
+};
+
+export default Projects;
---
+++ src/pages/Resume.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Resume = () => {
+    return (
+        <section id="resume">
+        <h2>Resume</h2>
+        <a href="./assets/Resume- Unedited.pages">Click here to see my resume</a>
+    </section>
+    );
+};
+
+export default Resume;
